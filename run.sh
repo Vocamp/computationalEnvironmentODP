520da01b2c4c4c02813dbd3e00c2495b40408bca
Jason Cheatham <j.cheatham@gmail.com>
2017-07-09 19:34:38 -0400

    Don't store transient data files

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ echo ">> Gathering processor names..."
 python3 bin/processors.py > data/processors.txt
 echo ">> Gathering processor architectures..."
 python3 bin/architectures.py > data/architectures.txt
+echo ">> Gathering kernel..."
+echo "" > data/kernels.txt
+echo ">> Gathering distros..."
+echo "" > data/distros.txt
 echo ">> Updating ontology..."
 java -jar bin/CompEnvUpdater.jar
 echo ">> Done."
