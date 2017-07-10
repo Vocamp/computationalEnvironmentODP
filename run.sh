ae09d04cc3383d4c6d74ce1ae8aac1ee1dc4c4cc
Michelle Cheatham <michelle.cheatham@gmail.com>
2017-07-10 17:38:51 -0400

    Add distros script

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ echo ">> Gathering processor architectures..."
 python3 bin/architectures.py > data/architectures.txt
 echo ">> Gathering OS kernel names..."
 python3 bin/kernels.py > data/kernels.txt
-echo ">> Gathering distros..."
-echo "" > data/distros.txt
+echo ">> Gathering OS distro names..."
+python3 bin/distros.py > data/distros.txt
 echo ">> Updating ontology..."
 java -jar bin/CompEnvUpdater.jar
 
