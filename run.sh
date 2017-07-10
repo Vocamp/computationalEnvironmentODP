c04c8507510ebb078cc1e940b80ea5e775243266
Jason Cheatham <j.cheatham@gmail.com>
2017-07-09 20:16:01 -0400

    Run script updates
    - Exit when any command fails
    - Detect ontology changes and offer to commit them

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 echo ">> Gathering processor names..."
 python3 bin/processors.py > data/processors.txt
 echo ">> Gathering processor architectures..."
@@ -10,4 +12,21 @@ echo ">> Gathering distros..."
 echo "" > data/distros.txt
 echo ">> Updating ontology..."
 java -jar bin/CompEnvUpdater.jar
+
+ontology=data/ComputationalEnvironment.rdf
+
+if [[ -n $(git diff-index --name-only HEAD -- $ontology) ]]; then
+	echo ">> The ontology has changed."
+	read -p ">> Commit it [y/n]? " yn
+	if [[ $yn == "y" ]]; then
+		echo ">> Committing..."
+		git commit "$ontology" -m "Update ontology"
+		echo ">> Pushing..."
+		# git push origin
+	else
+		echo ">> Discarding..."
+		git checkout "$ontology"
+	fi
+fi
+
 echo ">> Done."
